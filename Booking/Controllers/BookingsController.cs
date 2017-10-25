fb0209d0a9126bf5e95b370ab10d49dabeea9d74
Nikolaj Skovmose <nikolajskovmose@hotmail.com>
2017-10-25 18:43:25 +0200

    Added some models, and scaffolded

---
+++ Booking/Controllers/BookingsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Booking.Models;
+
+namespace Booking.Controllers
+{
+    public class BookingsController : Controller
+    {
+        private BookingEntities db = new BookingEntities();
+
+        // GET: Bookings
+        public ActionResult Index()
+        {
+            var bookings = db.Bookings.Include(b => b.Cars);
+            return View(bookings.ToList());
+        }
+
+        // GET: Bookings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bookings bookings = db.Bookings.Find(id);
+            if (bookings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookings);
+        }
+
+        // GET: Bookings/Create
+        public ActionResult Create()
+        {
+            ViewBag.FK_CarID = new SelectList(db.Cars, "CarId", "CarModel");
+            return View();
+        }
+
+        // POST: Bookings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "BookingID,FK_UserID,BookingDate,BookingStart,BookingEnd,FK_CarID")] Bookings bookings)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bookings.Add(bookings);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FK_CarID = new SelectList(db.Cars, "CarId", "CarModel", bookings.FK_CarID);
+            return View(bookings);
+        }
+
+        // GET: Bookings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bookings bookings = db.Bookings.Find(id);
+            if (bookings == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FK_CarID = new SelectList(db.Cars, "CarId", "CarModel", bookings.FK_CarID);
+            return View(bookings);
+        }
+
+        // POST: Bookings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BookingID,FK_UserID,BookingDate,BookingStart,BookingEnd,FK_CarID")] Bookings bookings)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bookings).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FK_CarID = new SelectList(db.Cars, "CarId", "CarModel", bookings.FK_CarID);
+            return View(bookings);
+        }
+
+        // GET: Bookings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Bookings bookings = db.Bookings.Find(id);
+            if (bookings == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bookings);
+        }
+
+        // POST: Bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Bookings bookings = db.Bookings.Find(id);
+            db.Bookings.Remove(bookings);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
